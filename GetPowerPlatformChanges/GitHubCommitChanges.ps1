cd503baa180a48374b24c240c26218026112408f
Anders Nørskov <andersg@microsoft.com>
2023-02-13 14:44:57 +0100

    changes to pull PP changes workflow

--- GetPowerPlatformChanges/GitHubCommitChanges.ps1
+++ GetPowerPlatformChanges/GitHubCommitChanges.ps1
@@ -7,7 +7,7 @@ Param(
     [string] $parentTelemetryScopeJson = '7b7d',
     [Parameter(HelpMessage = "Temporary location for files to be checked in", Mandatory = $false)]
     [string] $tempLocation,
-    [Parameter(HelpMessage = "The folder location of the PowerPlatform solution", Mandatory = $false)]
+    [Parameter(HelpMessage = "The relative folder location of the PowerPlatform solution", Mandatory = $false)]
     [string] $sourceLocation,
     [Parameter(HelpMessage = "Direct Commit (Y/N)", Mandatory = $false)]
     [bool] $directCommit
@@ -27,66 +27,62 @@ Function Copy-Files {
         [string]$Source,
         [Parameter(Mandatory = $true, HelpMessage = "The destination folder path")]
         [string]$Destination
-        )
+    )
+                
+    Write-Host "Copying files from $Source to $Destination"
 
-        Get-ChildItem $Source | ForEach-Object {
-            $destinationPath = Join-Path $Destination $_.Name
-            Copy-Item $_.FullName $destinationPath
-        }
+    Get-ChildItem $Source | ForEach-Object {
+        $destinationPath = Join-Path $Destination $_.Name
+        Copy-Item $_.FullName $destinationPath
     }
+}
     
-    Function CloneAndCommit {
-        [CmdletBinding()]
-        Param (
-            [Parameter(Mandatory = $true, HelpMessage = "The GitHub actor running the action")]
-            [string]$GitHubActor,
-            [Parameter(Mandatory = $true, HelpMessage = "The GitHub token running the action")]
-            [string]$GitHubToken,
-            [Parameter(Mandatory = $false, HelpMessage = "The branch name")]
-            [string]$GitHubBranch,
-            [Parameter(Mandatory = $false, HelpMessage = "The name of the PowerPlatform solution")]
-            [string]$PowerPlatformSolutionName
-            )
+Function CloneAndCommit {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true, HelpMessage = "The GitHub actor running the action")]
+        [string]$GitHubActor,
+        [Parameter(Mandatory = $true, HelpMessage = "The GitHub token running the action")]
+        [string]$GitHubToken,
+        [Parameter(Mandatory = $true, HelpMessage = "Indicates if direct commit")]
+        [bool]$DirectCommit,
+        [Parameter(Mandatory = $true, HelpMessage = "The name of the PowerPlatform solution")]
+        [string]$PowerPlatformSolutionName
+    )
             
-            # Import the helper script
-            . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+    # Import the helper script
+    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+    
+    $GitHubBranch = "";
+    if (!$DirectCommit) {
+        $GitHubBranch = [System.IO.Path]::GetRandomFileName()
+    }
             
-            # Determine the branch name
-            if (!$GitHubBranch) {
-                $GitHubBranch = [System.IO.Path]::GetRandomFileName()
-            }
+    # Clone the repository into a new folder
+    $ServerUrl = CloneIntoNewFolder -Actor $GitHubActor -Token $GitHubToken -Branch $GitHubBranch
             
-            # Clone the repository into a new folder
-            $ServerUrl = CloneIntoNewFolder -Actor $GitHubActor -Token $GitHubToken -Branch $GitHubBranch
+    $BaseFolder = (Get-Location).Path
+    Set-Location $BaseFolder
             
-            # Get the current
-            # Get the current location
-            $BaseFolder = (Get-Location).Path
+    Copy-Files -Source $tempLocation -Destination "$BaseFolder/$sourceLocation"
             
-            # Set the location to the base folder
-            Set-Location $BaseFolder
-            
-            # Call the Copy-Files function
-            Copy-Files -Source $tempLocation -Destination $sourceLocation
-            
-            # Commit from the new folder
-            CommitFromNewFolder -ServerUrl $ServerUrl -CommitMessage "Upadte solution: ($PowerPlatformSolutionName)" -Branch $GitHubBranch
-        }
+    # Commit from the new folder
+    CommitFromNewFolder -ServerUrl $ServerUrl -CommitMessage "Upadte solution: ($PowerPlatformSolutionName)" -Branch $GitHubBranch
+}
         
-        # IMPORTANT: No code that can fail should be outside the try/catch
-        try {
-            # Call the CloneAndCommit function
-            CloneAndCommit -GitHubActor $actor -GitHubToken $token -GitHubBranch $branch -PowerPlatformSolutionName $sourceLocation
-            TrackTrace -telemetryScope $telemetryScope
-        }
-        catch {
-            OutputError -message "CreateApp action failed.$([environment]::Newline)Error: $($_.Exception.Message)$([environment]::Newline)Stacktrace: $($_.scriptStackTrace)"
-            TrackException -telemetryScope $telemetryScope -errorRecord $_
-        }
-        finally {
-            CleanupAfterBcContainerHelper -bcContainerHelperPath $bcContainerHelperPath
-            if (Test-Path $tmpFolder) {
-                Remove-Item $tmpFolder -Recurse -Force
-            }
-        }
+# IMPORTANT: No code that can fail should be outside the try/catch
+try {
+    CloneAndCommit -GitHubActor $actor -GitHubToken $token -GitHubBranch $branch -PowerPlatformSolutionName $sourceLocation
+    TrackTrace -telemetryScope $telemetryScope
+}
+catch {
+    OutputError -message "Pull changes failed.$([environment]::Newline)Error: $($_.Exception.Message)$([environment]::Newline)Stacktrace: $($_.scriptStackTrace)"
+    TrackException -telemetryScope $telemetryScope -errorRecord $_
+}
+finally {
+    CleanupAfterBcContainerHelper -bcContainerHelperPath $bcContainerHelperPath
+    if (Test-Path $tmpFolder) {
+        Remove-Item $tmpFolder -Recurse -Force
+    }
+}
         

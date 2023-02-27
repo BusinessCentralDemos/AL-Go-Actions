f22be4cf813c7b63b0cebed9f3b6ab5d55905135
andersgMSFT <90765077+andersgMSFT@users.noreply.github.com>
2023-02-27 08:22:56 +0100

    update work flows

--- BuildPowerPlatform/updateSolutionFile.ps1
+++ BuildPowerPlatform/updateSolutionFile.ps1
@@ -21,18 +21,18 @@ function Update-VersionNode {
     )
 
     if ($appBuild -and $appRevision) {
-        $versionNode = $xmlFile.SelectSingleNode("//Version");
-        $versionNodeText = $versionNode.'#text';
+        $versionNode = $xmlFile.SelectSingleNode("//Version")
+        $versionNodeText = $versionNode.'#text'
         
-        $versionParts = $versionNodeText.Split('.');
+        $versionParts = $versionNodeText.Split('.')
         # Only update the last two parts of the version number - major and minor version should be set manually
-        $newVersionNumber = $versionParts[0] + '.' + $versionParts[1] + '.' + $appBuild + '.' + $appRevision;
+        $newVersionNumber = $versionParts[0] + '.' + $versionParts[1] + '.' + $appBuild + '.' + $appRevision
 
-        Write-Host "New version: "$newVersionNumber;
-        $versionNode.'#text' = $newVersionNumber;
+        Write-Host "New version: "$newVersionNumber
+        $versionNode.'#text' = $newVersionNumber
     }
     else {
-        Write-Host "Skipping version update since appBuild and appRevision are not set ($appBuild, $appRevision)";
+        Write-Host "Skipping version update since appBuild and appRevision are not set ($appBuild, $appRevision)"
     }
 }
 
@@ -44,20 +44,20 @@ function Update-ManagedNode {
         [xml] $xmlFile
     )
     
-    $managedValue = "0";
+    $managedValue = "0"
     if ($managed -eq "true") {
-        $managedValue = "1";
+        $managedValue = "1"
     }
 
-    $nodeWithName = $xmlFile.SelectSingleNode("//Managed");
-    Write-Host "Updating managed flag: "$managedValue;
-    $nodeWithName.'#text' = $managedValue;    
+    $nodeWithName = $xmlFile.SelectSingleNode("//Managed")
+    Write-Host "Updating managed flag: "$managedValue
+    $nodeWithName.'#text' = $managedValue    
 }
 
-Write-Host "Updating Power Platform solution ($solutionFolder)";
-$xmlFile = [xml](Get-Content $solutionFile);
+Write-Host "Updating Power Platform solution ($solutionFolder)"
+$xmlFile = [xml](Get-Content $solutionFolder)
 
-Update-VersionNode -appBuild $appBuild -appRevision $appRevision -xmlFile $xmlFile;
-Update-ManagedNode -managed $managed -xmlFile $xmlFile;
+Update-VersionNode -appBuild $appBuild -appRevision $appRevision -xmlFile $xmlFile
+Update-ManagedNode -managed $managed -xmlFile $xmlFile
         
-$xmlFile.Save($solutionFile);
+$xmlFile.Save($solutionFolder)
